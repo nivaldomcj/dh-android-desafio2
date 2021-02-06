15c87e0c44db52561a68c0d90714457dc895c932
Nivaldo Carvalho <nivaldo_j@outlook.com>
2021-02-06 11:49:45 -0300

    Added Register Screen

--- app/src/main/java/nivaldo/dh/exercise/android/auth/view/LoginFragment.kt
+++ app/src/main/java/nivaldo/dh/exercise/android/auth/view/LoginFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import nivaldo.dh.exercise.android.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
+
+    private fun initComponents() {
+        binding.btnRegister.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+            findNavController().navigate(action)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,4 +28,12 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initComponents()
+    }
 }
